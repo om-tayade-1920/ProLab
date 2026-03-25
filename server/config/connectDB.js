bca09f80a4568d6e06febae1559b72a03b3cc6da
om-tayade-1920 <omtayade86@gmail.com>
2026-03-25 22:36:27 +0530

    Update connectDB.js

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async (DB_URL) => {
     try {
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);;
         console.log('MongoDB Connected Successfully');
     } catch (error) {
         console.error('MongoDB Connection Failed:', error.message);
