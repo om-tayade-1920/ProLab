de4b9d69aa6596f7c1d8dec1bcafea0fe471a94b
om-tayade-1920 <omtayade86@gmail.com>
2026-03-25 23:04:47 +0530

    Update axios baseURL to use environment variable

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // or your hosted URL
+baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
   withCredentials: true,
 });
 
